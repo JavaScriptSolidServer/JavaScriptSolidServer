7548004035bf6c9e1c4fabcd19bc4dc7022ac27c
Melvin Carvalho <melvincarvalho@gmail.com>
2026-03-10 06:33:57 +0100

    Bump version to 0.0.97
    Adds Mastodon API, OAuth 2.0, and remoteStorage protocol.
    Also fixes stale 0.0.67 version strings in nodeinfo and instance endpoint.

--- src/ap/index.js
+++ src/ap/index.js
@@ -148,7 +148,7 @@ export async function activityPubPlugin(fastify, options = {}) {
         version: '2.1',
         software: {
           name: 'jss',
-          version: '0.0.67',
+          version: '0.0.97',
           repository: 'https://github.com/JavaScriptSolidServer/JavaScriptSolidServer'
         },
         protocols: ['activitypub', 'solid'],
